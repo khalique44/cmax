4929024529782f00ef2dc20bb7d4d5acd6ad43ba
Khalique Ahmed <khalique.ahmed3@gmail.com>
2025-04-07 02:23:10 +0500

    Changes in Master Baranch

--- database/migrations/2025_04_07_1743973605_create_posts_table.php
+++ database/migrations/2025_04_07_1743973605_create_posts_table.php
@@ -10,19 +10,17 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
 
-		$table->increments(id)->unsigned();
+		$table->increments('id')->unsigned();
 		$table->string('title',191)->nullable()->default('NULL');
-		;
 		$table->string('short_description',191)->nullable()->default('NULL');
-		$table->integer('category_id',)->nullable()->default('NULL');
+		$table->integer('category_id',)->nullable()->default(0);
 		$table->string('file_url',191)->nullable()->default('NULL');
 		$table->string('header_image',191)->nullable()->default('NULL');
 		$table->enum('status',['yes','no'])->default('yes');
-		$table->integer('position',)->nullable()->default('NULL');
-		$table->timestamp('deleted_at')->nullable()->default('NULL');
-		$table->timestamp('created_at')->nullable()->default('NULL');
-		$table->timestamp('updated_at')->nullable()->default('NULL');
-		$table->primary('id');
+		$table->integer('position',)->nullable()->default(0);
+		$table->timestamp('deleted_at')->nullable();
+		$table->timestamp('created_at')->nullable();
+		$table->timestamp('updated_at')->nullable();		
 
         });
     }
